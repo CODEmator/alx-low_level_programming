f1e53263a84dd41f2a9d2dffb409781a59f8f4db
philipnnaa <codematorng@gmail.com>
2022-03-11 23:27:57 -0800

    hello printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Return: Always 0
+ *
+ */
+init main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
