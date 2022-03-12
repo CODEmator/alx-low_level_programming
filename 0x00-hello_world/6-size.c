f969c2092d21a2dab111645bd8dffa0f70620861
philipnnaa <codematorng@gmail.com>
2022-03-12 01:57:43 -0800

    size six dot c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 	float afloat;
 
 	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("size of a int: %lu byte(s)\n"' sizeof(ainteger));
+	printf("size of a int: %lu byte(s)\n", sizeof(ainteger));
 	printf("size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
-
+	
 	return (0);
 }
