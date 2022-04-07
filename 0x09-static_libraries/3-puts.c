bbfbc8e2f6c31b048ab2e1df09eed8a9000f931b
philipnnaa <codematorng@gmail.com>
2022-04-06 17:59:12 -0700

    This has to work

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,20 @@
-ELF
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - This function prints a string
+ * to the standard output
+ * @str: pointer with the address value
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+
+		_putchar('\n');
+}
